da9de61bb19c41ee471a2470a2126f586a528570
bahati <bahatimuimi379@gmail.com>
2026-09-25 14:37:08 +0300

    attendee lab

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 //Write your code here
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
+// Task 3: Create a Function to Log Attendee Name
+function logAttendeeName(attendeeObj) {
+    console.log(attendeeObj.name);
+}
 
+// Task 4: Create a Function to Log Ticket Price
+function logTicketPrice(attendeeObj) {
+    console.log(attendeeObj.ticketPrice);
+}
 
+// Task 5: Create a Function to Update Ticket Type
+function updateTicketType(attendeeObj, newTicketType) {
+    attendeeObj.ticketType = newTicketType;
+}
 
+// Task 6: Create a Function to Update Ticket Price
+function updateTicketPrice(attendeeObj, newTicketPrice) {
+    attendeeObj.ticketPrice = newTicketPrice;
+}
+
+// Task 7: Create a Function to Remove the Event Property
+function removeEventProperty(attendeeObj) {
+    delete attendeeObj.event;
+}
+
+// Task 8: Create a Function to Add a Checked-in Property
+function addCheckedInProperty(attendeeObj) {
+    attendeeObj.checkedIn = true;
+}
 //Needed for the tests to work. Don't modify
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
